54a9aa5e0d4f1667c2bf486db1bee057938fe9ae
lucasnunessg <lucasnunespacheco@gmail.com>
2024-06-06 17:57:30 -0300

    mais implementações para entendimento

--- src/entities/Conta.ts
+++ src/entities/Conta.ts
@@ -1,4 +1,6 @@
 export type TipoConta = 'corrente' | 'poupança' | 'debito' | 'salario'
+export type Products = 'Livro' | 'Jornal' | 'Informática'
+export type CarroCompras = string[]
 
 export default class Conta {
  private saldo: number;
@@ -46,4 +48,41 @@ export default class Conta {
     }
     this.saldo -= valor
   }
+};
+
+export class Loja{
+  
+  products: Products;
+  price: number;
+  client: string;
+  saldoConta: number;
+  compras: CarroCompras;
+
+  constructor(products: Products, price: number, client: string, saldoConta: number) {
+    this.products = products;
+    this.price = price;
+    this.client = client;
+    this.saldoConta = saldoConta;
+    this.compras = [];
+    }
+
+  comprarProduto(clientComprador: string, saldoConta: number): string {
+    if (saldoConta <= 0) {
+      throw new Error('não pode comprar sem saldo');
+    }
+    if (saldoConta < this.price) {
+      throw new Error('saldo insuficiente para a compra');
+    }
+    this.client = clientComprador;
+    this.saldoConta = saldoConta - this.price;
+    return `${clientComprador} comprou ${this.products} por ${this.price}. Saldo restante: ${this.saldoConta}`;
+  }
+  addCarrinho (products: Products, client: string, compras: string): string{
+    if(client != this.client){
+      throw new Error('Cliente não pode comprar')
+    }
+    this.compras.push(products);
+    return `Produto ${products} adicionado ao carrinho`
+  }
 }
+
