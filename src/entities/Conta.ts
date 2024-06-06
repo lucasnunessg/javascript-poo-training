2087bb4808bd36ad27d39202b7b64ab0cdbd15e7
lucasnunessg <lucasnunespacheco@gmail.com>
2024-06-06 17:39:28 -0300

    implementando "banco" para entendimento de classes

---
+++ src/entities/Conta.ts
@@ -0,0 +1,49 @@
+export type TipoConta = 'corrente' | 'poupança' | 'debito' | 'salario'
+
+export default class Conta {
+ private saldo: number;
+ private tipo: TipoConta;
+ private nConta: string;
+ private agencia: string;
+ private titularidade: string;
+
+  constructor(
+    titularidade: string,
+    agencia: string,
+    nConta: string,
+    tipo: TipoConta = 'poupança',
+    saldo: number,
+  ){
+    this.titularidade = titularidade;
+    this.agencia = agencia;
+    this.nConta = nConta;
+    this.tipo = tipo;
+    this.saldo = saldo;
+  }
+
+  getSaldo(){
+    return this.saldo
+  }
+
+  depositar(valor: number){
+    if (valor <= 0){
+        throw new Error('Valores negativos  não são válidos!')
+    }
+    this.saldo += valor
+  }
+
+  transferir(contaDestino: Conta, valor: number) {
+    if(contaDestino.nConta === this.nConta){
+      throw new Error('Não é possível transferir para a mesma conta!')
+    } 
+    this.sacar(valor)
+    contaDestino.depositar(valor)
+  }
+
+  sacar(valor: number){
+    if(this.saldo <= 0){
+      throw new Error('Não pode sacar saldo negativo')
+    }
+    this.saldo -= valor
+  }
+}
