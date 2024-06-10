69b472bb9c46a9c4e86f26e2e3a75fbb96c9081b
lucasnunessg <lucasnunespacheco@gmail.com>
2024-06-10 17:59:11 -0300

    criando validações

--- src/entities/ContaPoupança.ts
+++ src/entities/ContaPoupança.ts
@@ -9,13 +9,23 @@ export  class ContaPoupança extends Conta {
 
   }
 
-  getRendimento(valor: number){
-
-    this.saldo *= (1 + this.rendimento)
-
+  sacar(valor: number){
+    if(valor <= 0 || valor > this.saldo) {
+    throw new Error('Não pode sacar abaixo do saldo')
+    }
+    super.sacar(valor);
   }
-    
+
+  getRendimento(valor: number){
+    this.saldo *= (1 + this.rendimento)
+  }
+
   
 }
+
+const newPoupança = new ContaPoupança('Lucas', '01239123', '0213', 'poupança', 1000)
+newPoupança.depositar(1000)
+newPoupança.sacar(2001)
+console.log(newPoupança)
 
 
