d8ac5f2255d100fbb57ec42ccb615f21ebc92067
lucasnunessg <lucasnunespacheco@gmail.com>
2024-06-11 14:24:35 -0300

    adicionando interface

--- src/entities/ContaPoupança.ts
+++ src/entities/ContaPoupança.ts
@@ -43,3 +43,19 @@ console.log(newPoupança)
 
 
 
+interface MyInterface {
+  myNumber: number,
+  myFunc(myParam: number): string 
+}
+
+export class MyClass implements MyInterface {
+constructor(public myNumber: number){
+
+}
+  myFunc(myParam: number): string {
+  return `myNumber + myParam: ${this.myNumber + myParam}`;
+}
+}
+
+const myObject = new MyClass(4)
+console.log('O resultado é: ', myObject)
